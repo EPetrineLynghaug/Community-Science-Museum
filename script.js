278003274bd12877810b2837fc44252ed0d2d001
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2023-11-14 09:55:32 +0100

    js W3 påbegynt.

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+// script.js-innholdet
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1; }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); // Endre bilde hvert 2. sekund
+}
